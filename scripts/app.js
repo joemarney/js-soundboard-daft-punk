704e90ca9cd61abb4341e7d3541b7ec23d2e9519
joemarney <marney.joe@gmail.com>
2024-09-08 11:00:34 +0100

    updated JS a bit

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,7 @@
-const afterSoundBite = document.querySelector("#after");
-const notAfter = document.querySelector(".container");
+const soundBite = document.querySelector(".container");
 
-const afterTrial = new Audio("../sounds/after.wav");
-
-afterSoundBite.addEventListener("click", (evt) => {
-  afterTrial.volume = 1;
-  afterTrial.play();
-});
-
-notAfter.addEventListener("click", (evt) => {
-  if (evt.target.id != "after") {
-    const audioElement = new Audio(`../sounds/${evt.target.id}.wav`);
-    audioElement.volume = 1;
-    audioElement.play();
-  }
+soundBite.addEventListener("click", (evt) => {
+  const audioElement = new Audio(`../sounds/${evt.target.id}.wav`);
+  audioElement.volume = 0.5;
+  audioElement.play();
 });
